5464c3bea01f62b8d3fe96b6d6ad20a8bce11775
Jagoba <jagobaperez92@opendeusto.es>
2017-04-10 18:10:31 +0200

    texture colliders added; old models removed; teleport from one point to another added

---
+++ BloodBath/Assets/Scripts/Entrance.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entrance : MonoBehaviour {
+
+	public float xOffset;
+	public float yOffset;
+	public GameObject destiny;
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.tag == "Player") {
+			Vector2 destinyPosition = destiny.GetComponent<Transform> ().position;
+			Vector2 newPosition = new Vector2 ();
+			newPosition.Set (
+				destinyPosition.x + xOffset,
+				destinyPosition.y + yOffset
+			);
+			collider.attachedRigidbody.position = newPosition;
+		}
+	}
+}
